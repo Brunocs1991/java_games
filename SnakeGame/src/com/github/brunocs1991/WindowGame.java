bdd8a651e97f0361688003b3aae5242c9cbc98f7
Brunocs1991 <brunocosta.1991@hotmail.com>
2021-12-24 13:27:16 -0300

    Auto moving snack and direction your move

--- SnakeGame/src/com/github/brunocs1991/WindowGame.java
+++ SnakeGame/src/com/github/brunocs1991/WindowGame.java
@@ -1,6 +1,6 @@
 package com.github.brunocs1991;
 
-import javax.swing.JFrame;
+import javax.swing.*;
 import java.awt.*;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
@@ -9,6 +9,11 @@ import java.util.TimerTask;
 
 public class WindowGame extends JFrame implements KeyListener {
 
+
+    private static final int UP = 0;
+    private static final int DOWN = 1;
+    private static final int LEFT = 2;
+    private static final int RIGHT = 3;
     private static final int WINDOWN_WIDTH = 400;
     private static final int WINDOWN_HEIGHT = 400;
     private static final int MATRIX_SIZE = 20;
@@ -17,6 +22,7 @@ public class WindowGame extends JFrame implements KeyListener {
     private static final int sizeOfSnackBody = WINDOWN_WIDTH / MATRIX_SIZE;
 
     private int headX = 3, headY = 4;
+    private int direction = RIGHT;
 
     WindowGame() {
         setSize(WINDOWN_WIDTH + PADDING * 2, WINDOWN_HEIGHT + PADDING * 2);
@@ -29,20 +35,37 @@ public class WindowGame extends JFrame implements KeyListener {
             public void run() {
                 moveSnake();
             }
-        },0, 400);
+        }, 0, 400);
     }
 
-    private void moveSnake(){
-        headX++;
+    private void moveSnake() {
+        switch (direction) {
+            case UP:
+                headY--;
+                break;
+            case DOWN:
+                headY++;
+                break;
+            case LEFT:
+                headX--;
+                break;
+            case RIGHT:
+                headX++;
+                break;
+        }
         repaint();
     }
+
     public static void main(String[] args) {
         new WindowGame();
     }
 
     @Override
     public void paint(Graphics g) {
-        super.paint(g);
+        //super.paint(g);
+        g.setColor(Color.WHITE);
+        g.fillRect(0, 0, getWidth(), getHeight());
+        g.setColor(Color.BLACK);
         for (int row = 0; row < matrix.length; row++) {
             for (int col = 0; col < matrix[row].length; col++) {
                 int x = col * sizeOfSnackBody + PADDING;
@@ -60,19 +83,18 @@ public class WindowGame extends JFrame implements KeyListener {
 
     @Override
     public void keyPressed(KeyEvent e) {
-        if (e.getKeyCode() == KeyEvent.VK_UP) {
-            headY--;
+        if (e.getKeyCode() == KeyEvent.VK_UP && direction != DOWN) {
+            direction = UP;
         }
-        if (e.getKeyCode() == KeyEvent.VK_DOWN) {
-            headY++;
+        if (e.getKeyCode() == KeyEvent.VK_DOWN && direction != UP) {
+            direction = DOWN;
         }
-        if (e.getKeyCode() == KeyEvent.VK_LEFT) {
-            headX--;
+        if (e.getKeyCode() == KeyEvent.VK_LEFT && direction != RIGHT) {
+            direction = LEFT;
         }
-        if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
-            headX++;
+        if (e.getKeyCode() == KeyEvent.VK_RIGHT && direction != LEFT) {
+            direction = RIGHT;
         }
-        repaint();
     }
 
     @Override
