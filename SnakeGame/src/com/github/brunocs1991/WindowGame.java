ef429b681a91d17d6d4fb52e2f0eb4b601be4a50
Brunocs1991 <brunocosta.1991@hotmail.com>
2021-12-24 15:09:30 -0300

    plot point and increment size list array

--- SnakeGame/src/com/github/brunocs1991/WindowGame.java
+++ SnakeGame/src/com/github/brunocs1991/WindowGame.java
@@ -1,9 +1,13 @@
 package com.github.brunocs1991;
 
+
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -21,9 +25,10 @@ public class WindowGame extends JFrame implements KeyListener {
     private static final int[][] matrix = new int[MATRIX_SIZE][MATRIX_SIZE];
     private static final int sizeOfSnackBody = WINDOWN_WIDTH / MATRIX_SIZE;
 
-    private int headX = 3, headY = 4;
-    private static final int[] bodyX = new int[4];
-    private static final int[] bodyY = new int[4];
+
+    private final List<Integer> bodyX = new ArrayList<>();
+    private final List<Integer> bodyY = new ArrayList<>();
+    private int headX = 6, headY = 4;
     private int direction = RIGHT;
 
     WindowGame() {
@@ -40,26 +45,27 @@ public class WindowGame extends JFrame implements KeyListener {
                 moveSnake();
             }
         }, 0, 400);
-        bodyX[0] = 5;
-        bodyX[1] = 4;
-        bodyX[2] = 3;
-        bodyX[3] = 2;
+        bodyX.add(5);
+        bodyX.add(4);
 
-        bodyY[0] = 4;
-        bodyY[1] = 4;
-        bodyY[2] = 4;
-        bodyY[3] = 4;
+        bodyY.add(4);
+        bodyY.add(4);
 
+        matrix[11][11] = 1;
+        matrix[11][12] = 1;
+        matrix[11][13] = 1;
     }
 
     private void moveSnake() {
         //Update snack body
-        for (int i  = 3; i > 0; i --){
-            bodyX[i] = bodyX[i -1];
-            bodyY[i] = bodyY[i -1];
+        int oldTailX = bodyX.get(bodyX.size() - 1);
+        int oldTailY = bodyY.get(bodyY.size() - 1);
+        for (int i  = bodyX.size() -1; i > 0; i --){
+            bodyX.set(i, bodyX.get(i - 1));
+            bodyY.set(i, bodyY.get(i - 1));
         }
-        bodyX[0] = headX;
-        bodyY[0] = headY;
+        bodyX.set(0, headX);
+        bodyY.set(0, headY);
         switch (direction) {
             case UP:
                 headY--;
@@ -73,6 +79,11 @@ public class WindowGame extends JFrame implements KeyListener {
             case RIGHT:
                 headX++;
                 break;
+        }
+        if(matrix[headY][headX] == 1){
+            bodyX.add(oldTailX);
+            bodyY.add(oldTailY);
+            matrix[headY][headX] = 0;
         }
         repaint();
     }
@@ -88,17 +99,24 @@ public class WindowGame extends JFrame implements KeyListener {
         g.setColor(Color.GREEN);
         g.fillRect(headX * sizeOfSnackBody + PADDING, headY * sizeOfSnackBody + PADDING, sizeOfSnackBody, sizeOfSnackBody);
 
-        for (int i = 0; i < 4; i ++){
+        for (int i = 0; i < bodyX.size(); i ++){
             //Draw the snake body
             g.setColor(Color.RED);
-            g.fillRect(bodyX[i] * sizeOfSnackBody + PADDING,bodyY[i] * sizeOfSnackBody + PADDING, sizeOfSnackBody, sizeOfSnackBody);
+            g.fillRect(bodyX.get(i) * sizeOfSnackBody + PADDING,bodyY.get(i) * sizeOfSnackBody + PADDING, sizeOfSnackBody, sizeOfSnackBody);
         }
         g.setColor(Color.BLACK);
         for (int row = 0; row < matrix.length; row++) {
             for (int col = 0; col < matrix[row].length; col++) {
                 int x = col * sizeOfSnackBody + PADDING;
                 int y = row * sizeOfSnackBody + PADDING;
-                g.drawRect(x, y, sizeOfSnackBody, sizeOfSnackBody);
+                if(matrix[row][col] == 1){
+                    g.setColor(Color.BLUE);
+                    g.fillRect(x, y, sizeOfSnackBody, sizeOfSnackBody);
+                }else{
+                    g.setColor(Color.BLACK);
+                    g.drawRect(x, y, sizeOfSnackBody, sizeOfSnackBody);
+                }
+
             }
         }
 
