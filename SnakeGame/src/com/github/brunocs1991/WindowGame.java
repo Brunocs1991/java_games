0154af986de0c3de4f504b0d3371152ac3a4b251
Brunocs1991 <brunocosta.1991@hotmail.com>
2021-12-24 12:25:46 -0300

    Finalizando grade

--- SnakeGame/src/com/github/brunocs1991/WindowGame.java
+++ SnakeGame/src/com/github/brunocs1991/WindowGame.java
@@ -1,6 +1,36 @@
 package com.github.brunocs1991;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class WindowGame extends JFrame {
+
+    private static final int WINDOWN_WIDTH = 400;
+    private static final int WINDOWN_HEIGHT = 400;
+    private static final int MATRIX_SIZE = 20;
+    private static final int PADDING = 50;
+    private static final int[][] matrix = new int[MATRIX_SIZE][MATRIX_SIZE];
+    private static final int sizeOfSnackBody = WINDOWN_WIDTH / MATRIX_SIZE;
+
+    WindowGame() {
+        setSize(WINDOWN_WIDTH + PADDING * 2, WINDOWN_HEIGHT + PADDING * 2);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new WindowGame();
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        super.paint(g);
+        for (int row = 0; row < matrix.length; row++) {
+            for (int col = 0; col < matrix[row].length; col++) {
+                int x  = col * sizeOfSnackBody + PADDING;
+                int y = row * sizeOfSnackBody + PADDING;
+                g.drawRect(x, y, sizeOfSnackBody, sizeOfSnackBody);
+            }
+        }
+    }
 }
